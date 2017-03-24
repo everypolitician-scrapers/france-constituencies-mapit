81fb376e559e988e2d5b170c972ca5c6d4788a99
Chris Mytton <chrismytton@gmail.com>
2017-03-24 14:30:56 +0100

    Add Mapit Areas JSON scraper

--- scraper.rb
+++ scraper.rb
@@ -4,10 +4,7 @@ require 'pry'
 require 'require_all'
 require 'scraped'
 require 'scraperwiki'
-
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
-require 'scraped_page_archive/open-uri'
+require 'json'
 
 # require_rel 'lib'
 
@@ -15,3 +12,44 @@ def scrape(h)
   url, klass = h.to_a.first
   klass.new(response: Scraped::Request.new(url: url).response)
 end
+
+class MapitArea
+  include FieldSerializer
+
+  def initialize(area)
+    @area = area
+  end
+
+  field :id do
+    area[:id]
+  end
+
+  field :name do
+    area[:name]
+  end
+
+  field :osm_rel do
+    area[:codes][:osm_rel]
+  end
+
+  private
+
+  attr_reader :area
+end
+
+class MapitAreas < Scraped::Document
+  field :areas do
+    json.values.map { |area| MapitArea.new(area) }.map(&:to_h)
+  end
+
+  private
+
+  def json
+    @json ||= JSON.parse(response.body, symbolize_names: true)
+  end
+end
+
+areas = scrape('http://global.mapit.mysociety.org/areas/OCL' => MapitAreas).to_h[:areas]
+
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
+ScraperWiki.save_sqlite([:id], areas)
